36b2a7b80eddf50b8b73d23ea70d01503a01e560
Dalvin <66400608+d41vin@users.noreply.github.com>
2025-02-14 21:33:38 +0800

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import UserProfile from "@/components/UserProfile";
+import { Button } from "@/components/ui/button";
+import ActionButtons from "@/components/ActionButtons";
+import PayOrRequest from "@/components/PayOrRequest";
 
 export default function HomePage() {
   return (
@@ -9,11 +14,23 @@ export default function HomePage() {
       {/* nav */}
       <div className="relative flex-1">
         <Search className="pointer-events-none absolute left-3 top-2.5 h-4 w-4 text-muted-foreground" />
-        <Input placeholder="Search users" className="rounded-full pl-9" />
+        <Input
+          placeholder="Find friends or businesses"
+          className="rounded-full pl-9"
+        />
       </div>
 
       {/* header */}
       <UserProfile />
+
+      {/* buttons */}
+      <Button variant="secondary">Receive</Button>
+      <Button variant="default">Send/Request</Button>
+      <Button variant="outline">Split</Button>
+
+      <ActionButtons />
+
+      <PayOrRequest />
 
       {/* tabs */}
       <h1>Home page</h1>
